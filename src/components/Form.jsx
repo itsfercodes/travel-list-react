555b6e5ea54f7ff0bf46eddeb31d930a9ed57a2b
Fernando <itsfercodes@outlook.com>
2023-08-16 13:21:54 -0600

    Add state to form elements and creation on new Item object (Pending add it to the PAcakingList component)

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,45 @@
+import { useState } from 'react';
+
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    // Guard
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now()
+    };
+    setDescription('');
+    setQuantity(1);
+    console.log(newItem);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?🤔</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button type="submit">Add</button>
     </form>
   );
