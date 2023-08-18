a2c55e7fc0bd8e3908e7ee5eeb8fdf2cc095c279
Fernando <itsfercodes@outlook.com>
2023-08-18 14:19:54 -0600

    Add sort list items options

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,11 +1,31 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import Item from './Item';
 
+const INPUT = 'input';
+const DESCRIPTION = 'description';
+const PACKED = 'packed';
+
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState(INPUT);
+
+  let sortedItems;
+  if (sortBy === INPUT) sortedItems = items;
+  if (sortBy === DESCRIPTION) {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === PACKED) {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -14,6 +34,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value={INPUT}>Sort by input order</option>
+          <option value={DESCRIPTION}>Sort by description</option>
+          <option value={PACKED}>Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
